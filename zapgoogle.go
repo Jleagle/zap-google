b275b419948e3a161f1c92792dffe7b16fd6547d
Jleagle <jimeagle@gmail.com>
2026-09-02 13:04:22 +0100

    Google error reporting compatibility

--- zapgoogle.go
+++ zapgoogle.go
@@ -3,121 +3,100 @@ package zapgoogle
 import (
 	"context"
 	"encoding/json"
-	"io"
-	"sync"
+	"strings"
 
 	"cloud.google.com/go/logging"
+	"cloud.google.com/go/logging/apiv2/loggingpb"
+	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
-	"google.golang.org/api/option"
 )
 
 //goland:noinspection GoUnusedExportedFunction
-func NewCore(projectID string, synchronous bool, clientOps []option.ClientOption, loggerOps []logging.LoggerOption) (zapcore.Core, error) {
-
-	ctx := context.Background()
-
-	googleClient, err := logging.NewClient(ctx, projectID, clientOps...)
-	if err != nil {
-		return nil, err
-	}
-
-	return NewCoreWithClient(googleClient, synchronous, loggerOps), nil
-}
-
-//goland:noinspection GoUnusedExportedFunction
-func NewCoreWithClient(client *logging.Client, synchronous bool, loggerOps []logging.LoggerOption) zapcore.Core {
+func New(ctx context.Context, logger *logging.Logger, synchronous bool) zapcore.Core {
 
 	return &googleCore{
-		client:      client,
-		context:     context.Background(),
-		loggers:     map[string]*logging.Logger{},
+		logger:      logger,
+		context:     ctx,
 		synchronous: synchronous,
-		loggerOps:   loggerOps,
-		mu:          &sync.RWMutex{},
-
-		encoder: zapcore.NewJSONEncoder(googleEncoderConfig()),
-		output:  zapcore.AddSync(io.Discard),
+		encoder: zapcore.NewJSONEncoder(zapcore.EncoderConfig{
+			MessageKey:     "message",
+			LevelKey:       "severity",
+			TimeKey:        "timestamp",
+			NameKey:        "logName",
+			CallerKey:      "caller",
+			StacktraceKey:  "stack_trace",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.CapitalLevelEncoder,
+			EncodeTime:     zapcore.RFC3339TimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		}),
 	}
 }
 
 type googleCore struct {
-	client      *logging.Client
+	logger      *logging.Logger
 	context     context.Context
-	loggers     map[string]*logging.Logger
 	synchronous bool
-	loggerOps   []logging.LoggerOption
-	mu          *sync.RWMutex
-
-	encoder zapcore.Encoder
-	output  zapcore.WriteSyncer
+	encoder     zapcore.Encoder
 }
 
-func (g *googleCore) clone() *googleCore {
-
-	return &googleCore{
-		client:      g.client,
-		context:     g.context,
-		loggers:     g.loggers,
-		synchronous: g.synchronous,
-		mu:          g.mu,
-
-		encoder: g.encoder.Clone(),
-		output:  g.output,
-	}
-}
-
-func (g *googleCore) getLogger(name string) *logging.Logger {
-
-	g.mu.RLock()
-	val, ok := g.loggers[name]
-	g.mu.RUnlock()
-	if ok {
-		return val
-	}
-
-	g.mu.Lock()
-	defer g.mu.Unlock()
-
-	// Double check
-	if val, ok = g.loggers[name]; ok {
-		return val
-	}
-
-	g.loggers[name] = g.client.Logger(name, g.loggerOps...)
-
-	return g.loggers[name]
-}
-
-func (g *googleCore) Enabled(level zapcore.Level) bool {
+func (g *googleCore) Enabled(_ zapcore.Level) bool {
 	return true
 }
 
 func (g *googleCore) With(fields []zapcore.Field) zapcore.Core {
 
-	clone := g.clone()
+	clone := &googleCore{
+		logger:      g.logger,
+		context:     g.context,
+		synchronous: g.synchronous,
+		encoder:     g.encoder.Clone(),
+	}
+
 	for k := range fields {
 		fields[k].AddTo(clone.encoder)
 	}
+
 	return clone
 }
 
 func (g *googleCore) Check(entry zapcore.Entry, checkedEntry *zapcore.CheckedEntry) *zapcore.CheckedEntry {
-	return checkedEntry.AddCore(entry, g)
+
+	if g.Enabled(entry.Level) {
+		return checkedEntry.AddCore(entry, g)
+	}
+
+	return checkedEntry
 }
 
 func (g *googleCore) Write(entry zapcore.Entry, fields []zapcore.Field) error {
 
+	if entry.Level >= zapcore.ErrorLevel {
+		newFields := make([]zapcore.Field, 0, len(fields)+1)
+		newFields = append(newFields, fields...)
+		newFields = append(newFields, zap.String("@type", "type.googleapis.com/google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent"))
+		fields = newFields
+
+		if entry.Stack != "" {
+			// Format zap's stack trace to look like a standard Go panic stack trace
+			// so Error Reporting can parse it correctly.
+			stack := entry.Stack
+			lines := strings.Split(stack, "\n")
+			for i, line := range lines {
+				if !strings.HasPrefix(line, "\t") && line != "" {
+					if !strings.HasSuffix(line, ")") {
+						lines[i] = line + "()"
+					}
+				}
+			}
+			entry.Stack = "goroutine 1 [running]:\n" + strings.Join(lines, "\n")
+		}
+	}
+
 	buf, err := g.encoder.EncodeEntry(entry, fields)
 	if err != nil {
 		return err
-	}
-
-	var payload interface{}
-	var payloadMap map[string]interface{}
-	if err := json.Unmarshal(buf.Bytes(), &payloadMap); err == nil {
-		payload = payloadMap
-	} else {
-		payload = buf.String()
 	}
 
 	var level logging.Severity
@@ -138,52 +117,37 @@ func (g *googleCore) Write(entry zapcore.Entry, fields []zapcore.Field) error {
 	case zapcore.FatalLevel:
 		level = logging.Alert
 	default:
-		level = logging.Error
+		level = logging.Default
 	}
+
+	b := make([]byte, buf.Len())
+	copy(b, buf.Bytes())
+
+	buf.Free()
 
 	googleEntry := logging.Entry{
 		Timestamp: entry.Time,
 		Severity:  level,
-		Payload:   payload,
+		Payload:   json.RawMessage(b),
 	}
 
-	logger := g.getLogger(entry.LoggerName)
+	if entry.Caller.Defined {
+		googleEntry.SourceLocation = &loggingpb.LogEntrySourceLocation{
+			File:     entry.Caller.File,
+			Line:     int64(entry.Caller.Line),
+			Function: entry.Caller.Function,
+		}
+	}
 
 	if g.synchronous {
-		return logger.LogSync(g.context, googleEntry)
+		return g.logger.LogSync(g.context, googleEntry)
 	}
 
-	logger.Log(googleEntry)
+	g.logger.Log(googleEntry)
 
 	return nil
 }
 
 func (g *googleCore) Sync() error {
-
-	for _, logger := range g.loggers {
-
-		err := logger.Flush()
-		if err != nil {
-			return err
-		}
-	}
-
-	return g.output.Sync()
-}
-
-// https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry
-func googleEncoderConfig() zapcore.EncoderConfig {
-	return zapcore.EncoderConfig{
-		MessageKey:     "message",
-		LevelKey:       "severity",
-		TimeKey:        "timestamp",
-		NameKey:        "logName",
-		CallerKey:      "caller",
-		StacktraceKey:  "stack_trace",
-		LineEnding:     zapcore.DefaultLineEnding,
-		EncodeLevel:    zapcore.CapitalLevelEncoder,
-		EncodeTime:     zapcore.RFC3339TimeEncoder,
-		EncodeDuration: zapcore.SecondsDurationEncoder,
-		EncodeCaller:   zapcore.ShortCallerEncoder,
-	}
+	return g.logger.Flush()
 }
